1a9d465c439a069130c52a3e442b1db323a2bcd5
Adriasu <adsuarez09@gmail.com>
2026-01-27 00:19:46 +0100

    feat: implement About section with responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/layout/Header";
+import { About } from "@/components/sections/About/About";
 import { Hero } from "@/components/sections/Hero/Hero";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
       <Header />
       <main className="bg-base">
         <Hero />
+        <About />
       </main>
     </>
   );
